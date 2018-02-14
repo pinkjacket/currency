077b22cc61c3c5edfd4dd80f31351c9ac29aa4c9
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-02-13 23:42:56 -0800

    first test failing

---
+++ test/money.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+const assert = require('assert');
+import * as Money from "../src/money.js";
+
+describe ("Money", function() {
+
+  describe("moneyTrade", function(){
+    it("should convert dollars to pounds", function(){
+      expect(Money.moneyTrade(1).to.equal(.72));
+    });
+  });
+
+})
