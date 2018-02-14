ba322769ae239e114c233a98720280bb8205e2b2
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-02-14 00:33:53 -0800

    third conversion test passing

--- test/money.test.js
+++ test/money.test.js
@@ -16,4 +16,10 @@ describe ("Money", function() {
     });
   });
 
+  describe("moneyTrade", function(){
+    it("should convert dollars to euros", function(){
+      expect(Money.moneyTrade(1, "usdtoeuro")).to.equal(.81);
+    });
+  });
+
 })
