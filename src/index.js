f43ce3fe336594cc3c2a090c13825f561e580591
James Donlan <jacketlizard@live.com>
2018-02-14 15:11:26 -0800

    beginning front end, basic structures for index files laid out

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./templates/styles.css";
+import * as Money from "../src/money.js";
+
+//remember to stitch together inputs so exchange rates can still work
+
+$(document).ready(function(){
+  $("#cashForm").submit(function(event){
+    event.preventDefault();
+
+    //input into variables here
+
+    //send to backend here
+
+    //output here
+  })
+})
